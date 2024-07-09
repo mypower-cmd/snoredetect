a8f4da6080a645d3a1f45979e34b7fa0f0acb900
liu <shuibin_mail@foxmail.com>
2024-07-09 21:45:36 +0800

    fix tfdata bug

--- code/train.py
+++ code/train.py
@@ -7,7 +7,8 @@ from tensorflow.keras.callbacks import (ReduceLROnPlateau, TensorBoard)
 import sys
 from process_data import gen_data_batch
 from model import *
-
+import os
+import pandas as pd
 sys.path.append("..")
 model = cnn_model()
 # model = cnn_rnn_model()
@@ -29,7 +30,8 @@ class MySetLR(tf.keras.callbacks.Callback):
             (epoch + 1, lr))
 
 
-def train_and_val():
+#配置GPU，限制GPU内存增长
+def GPU_Config():
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
         try:
@@ -42,35 +44,70 @@ def train_and_val():
         except RuntimeError as e:
             # Memory growth must be set before GPUs have been initialized
             print(e)
-    train_dataset = gen_data_batch(cfg.train.dataset,
+
+def train_and_val():
+    # 配置GPU
+    GPU_Config()
+    list_num_samples = np.load(cfg.num_samples_file_name)  # 读取
+    cfg.num_samples = list_num_samples[0]
+    print("num_samples:", cfg.num_samples )
+
+    dataset = gen_data_batch(cfg.dataset_path,
                                    cfg.batch_size,
-                                   cfg.epochs,
+                                   1,
                                    is_training=True)
-    val_dataset = gen_data_batch(cfg.val.dataset,
-                                 cfg.batch_size,
-                                 is_training=False)
+
+    train_num = (cfg.train_num_samples*(1 - cfg.val_rate + cfg.test_rate))//cfg.batch_size
+    val_num = (cfg.train_num_samples * cfg.val_rate)//cfg.batch_size
+    test_num = (cfg.train_num_samples * cfg.test_rate)//cfg.batch_size
+    train_data = dataset.take(train_num)
+    temp_data = dataset.skip(train_num)
+    val_data = temp_data.take(val_num)
+    test_data = temp_data.skip(val_num)
+
     optimizer = tf.keras.optimizers.Adam(lr=cfg.train.learning_rate)
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
+    # callbacks = [
+    #     ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=1),
+    #     MySetLR(),
+    #     TensorBoard(log_dir=cfg.log_dir, histogram_freq=1),
+    #
+
+    #创建保存文件路径
+    folder_path = "../model/"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    model_path = "/home/lsb/project/Sleep_Stage/model/" + "sleep_classify_" + datetime.datetime.now(
+    ).strftime("%m%d_%H%M%S") + ".h5"
+
+    # 配置回调函数
     callbacks = [
-        ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, verbose=1),
-        MySetLR(),
-        TensorBoard(log_dir=cfg.log_dir, histogram_freq=1),
+        # ReduceLROnPlateau(monitor='loss', factor=0.9, patience=10, verbose=1),
+        tf.keras.callbacks.ModelCheckpoint(model_path, monitor='val_accuracy', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
     ]
 
-    history = model.fit(train_dataset,
+    history = model.fit(train_data,
                         epochs=cfg.epochs,
+                        batch_size=cfg.batch_size,
                         callbacks=callbacks,
-                        validation_data=val_dataset,
-                        steps_per_epoch=cfg.train.num_samples // cfg.batch_size,
-                        validation_steps=cfg.val.num_samples // cfg.batch_size)
+                        validation_data=val_data,
+                        steps_per_epoch=train_num,
+                        validation_steps=val_num)
+    #创建保存文件路径
+    folder_path = "../results/"
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    pd.DataFrame(history.history).to_csv('../results/training_log.csv', index=True)
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
+    # 画出训练和测试的准确率和损失值
     epochs_range = range(cfg.epochs)
     plt.figure()
     plt.subplot(1, 2, 1)
@@ -85,19 +122,23 @@ def train_and_val():
     plt.legend(loc='upper right')
     plt.title('Training and Validation Loss')
 
-    result_png = "./result_img/" + "result_" + datetime.datetime.now(
+    # 保存图片12
+    result_png = "../results/" + "result_" + datetime.datetime.now(
     ).strftime("%m%d_%H%M%S") + ".png"
     plt.savefig(result_png)
     plt.show()
 
-    test_loss, test_acc = model.evaluate(val_dataset,
-                                         steps=(cfg.val.num_samples //
-                                                cfg.batch_size),
-                                         verbose=1)
-    print("test_acc =", test_acc)
-    print("test_loss =", test_loss)
 
-    model.save(cfg.save_mode_path)
+
+    print('-----------test--------')
+    test_loss, test_acc = model.evaluate(test_data, steps=test_numt, verbose=1)
+    print('测试准确率：', test_acc)
+    print('测试损失', test_loss)
+
+    # 模型保存
+    final_model_path = "../model/" + "sleep_classify_" + datetime.datetime.now(
+    ).strftime("%m%d_%H%M%S") + "final.h5"
+    model.save(final_model_path)
 
 
 if __name__ == '__main__':
